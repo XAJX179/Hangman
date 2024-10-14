d60d205edd62fab4c31b193db1277a6c7f14aab9
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-10-15 00:30:43 +0530

    Feat : randomly selects word

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,23 @@
 class Game
   def start
     file = load_word_file
-    puts file.read
+    random_word = random_word(file)
+    puts "return : #{random_word}"
   end
 
+  private
+
+  # load words file
   def load_word_file
     File.open('google-10000-english-no-swears.txt')
+  rescue StandardError
+    puts 'word file does not exists.'
+  end
+
+  # return random word of length 5-12 from loaded file
+  def random_word(file)
+    words = file.readlines(chomp: true) # chomps \n
+    words.shuffle! until words[0].length >= 5 && words[0].length <= 12
+    words[0]
   end
 end
