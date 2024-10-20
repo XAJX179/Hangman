4c92061165f91fac3edc597fb5b5e4323e9467d8
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-10-20 17:06:11 +0530

    Feat : Player class Args with default values

--- lib/player.rb
+++ lib/player.rb
@@ -3,12 +3,13 @@
 # Player playing the game
 class Player
   attr_accessor :incorrect_letters
-  attr_reader :guessed_letters, :correct_letters
+  attr_reader :guessed_letters, :correct_letters, :secret_word
 
-  def initialize
-    @guessed_letters = []
-    @correct_letters = []
-    @incorrect_letters = []
+  def initialize(guessed_letters = [], correct_letters = [], incorrect_letters = [])
+    @guessed_letters = guessed_letters
+    @correct_letters = correct_letters
+    @incorrect_letters = incorrect_letters
+    @secret_word = ''
   end
 
   def user_input
